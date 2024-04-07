c36bb2c58f49a75bd12aafab7661d1f201125612
sauravZope <zopesaurav3564@gmail.com>
2024-04-07 23:00:17 +0530

    updated

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -15,8 +15,8 @@ app.use(cors());
 
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qbku1ed.mongodb.net/?retryWrites=true&w=majority`;
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qbku1ed.mongodb.net/?retryWrites=true&w=majority`;
+const uri =process.env.MONGO_URL;
   
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
